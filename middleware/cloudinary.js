755fbc87c9b7772d2085711365ad203bee4e9db5
omermazorgroup <92123730+omermazorgroup@users.noreply.github.com>
2022-03-31 15:52:37 +0300

    Update cloudinary.js

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dwsdrdv3w",
-  api_key:"281967744783166",
-  api_secret:"6iXWOqcmbBsPSwNyMpFXVv2512M"
+  api_key: process.env.CLOUDINARY_API_KEY
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
